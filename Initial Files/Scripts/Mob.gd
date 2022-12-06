368b1b68af6cad2fc87a864f0d80ffe1bc7ec379
amandodaw <47083109+amandodaw@users.noreply.github.com>
2022-12-06 11:06:40 +0100

    Arreglada señal ambigua
    Hecho con is_queued_for_deletion()

--- Initial Files/Scripts/Mob.gd
+++ Initial Files/Scripts/Mob.gd
@@ -1,19 +1,27 @@
 extends RigidBody2D
 signal damage
+signal sword_increment
 
 func _ready():
 	connect("damage", get_parent(), "get_hit")
-	connect("damage", get_parent().get_node("Player"), "sword_increment")
-
-func _on_Area2D_body_entered(body):
-	if body != self:
-		queue_free()
-
+	connect("sword_increment", get_parent().get_node("Player"), "sword_increment")
 
 func _on_Area2D_area_entered(area):
+	emit_signal("sword_increment")
 	queue_free()
+	print(3)
+	yield(get_tree().create_timer(1.0), "timeout")
+	print(2)
+	yield(get_tree().create_timer(1.0), "timeout")
+	print(1)
+	yield(get_tree().create_timer(1.0), "timeout")
+
+
+
 
 
 func _on_VisibilityNotifier2D_screen_exited():
+	if is_queued_for_deletion():
+		return
 	emit_signal("damage")
 	queue_free()
